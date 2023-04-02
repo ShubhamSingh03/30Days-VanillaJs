bf023de9ee0de11264b38fa1a9ad095ea04965cd
Shubham Singh <53564021+ShubhamSingh03@users.noreply.github.com>
2023-04-02 17:20:07 +0530

    day-02 gradient color gen. app done

---
+++ 02 - Gradient Color Generator/Readme.md
@@ -0,0 +1,13 @@
+# Gradient Color Generator
+
+The Gradient Color Generator App is a web-based tool built with HTML, CSS, and vanilla JavaScript. It allows users to generate custom gradient color combinations for their web design projects quickly and easily.
+
+The app features a user-friendly interface with color pickers, a preview pane. Once the user has created their desired gradient, they can copy the CSS code and paste it into their project.
+
+#### [Live Link]()
+
+#### [Youtube Demo]()
+
+<br/>
+
+![Demo-screenshot-1](./assests/img/gradientGenerator.png)
---
+++ 02 - Gradient Color Generator/assests/js/gradient.js
@@ -0,0 +1,60 @@
+// selecting dom values
+let gradientColorOne = document.getElementById("color_one");
+let gradientColorTwo = document.getElementById("color_two");
+let gradientColorDirection = "to top";
+let outputcode = document.getElementById("output-value");
+
+// event listeners for buttons list
+let buttons = document.querySelectorAll(".buttons button");
+buttons.forEach((button) => {
+  button.addEventListener("click", function () {
+    setGradientColorDirection(this.getAttribute("data-direction"), this);
+  });
+});
+
+const submitBtn = document.getElementById("submit");
+submitBtn.addEventListener("click", generateGradientColor);
+
+const copyBtn = document.getElementById("copy");
+copyBtn.addEventListener("click", copyToClipboard);
+
+// set gradient directions function
+function setGradientColorDirection(value, _this) {
+  let direction = document.querySelectorAll(".buttons button");
+  direction.forEach((e) => {
+    e.classList.remove("active");
+  });
+  _this.classList.add("active");
+  gradientColorDirection = value;
+}
+
+// generate color
+function generateGradientColor() {
+  outputcode.value = `background-image: linear-gradient(${gradientColorDirection}, ${gradientColorOne.value}, ${gradientColorTwo.value});`;
+  document.getElementById(
+    "body"
+  ).style.backgroundImage = `linear-gradient(${gradientColorDirection}, ${gradientColorOne.value}, ${gradientColorTwo.value})`;
+}
+
+// copy to clipboard & toast
+function copyToClipboard() {
+  navigator.clipboard.writeText(outputcode.value);
+  toastr["success"]("Copied to Clipboard", "Copied");
+
+  toastr.options = {
+    closeButton: false,
+    debug: false,
+    newestOnTop: true,
+    progressBar: false,
+    positionClass: "toast-top-center",
+    preventDuplicates: false,
+    showDuration: "200",
+    hideDuration: "500",
+    timeOut: "2500",
+    extendedTimeOut: "1000",
+    showEasing: "swing",
+    hideEasing: "linear",
+    showMethod: "fadeIn",
+    hideMethod: "fadeOut",
+  };
+}
